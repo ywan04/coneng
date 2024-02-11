2dd26c30510bdf532b8fd8ba7cc0218fd046d728
Ivan Bereza <iv.bereza04@gmail.com>
2024-02-11 19:19:58 +0200

    fix configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -226,6 +226,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
